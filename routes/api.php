95041c73d286251e5b60db671446cc9b55cb02e6
DanielBarrientos <danielbarrientos0802@gmail.com>
2022-03-25 16:50:42 -0500

    Modificando método de login

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/login',[AuthController::class,'login']);
 Route::post('/users/register',[UserController::class,'register']);
+
